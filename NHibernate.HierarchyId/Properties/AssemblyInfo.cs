3defbe603844c8d33df51dc9e91f0af273118751
Artur Kraev <ravenox@gmail.com>
2013-07-17 17:12:16 +0400

    Added nuget package creation ( fixes: #5 )

--- NHibernate.HierarchyId/Properties/AssemblyInfo.cs
+++ NHibernate.HierarchyId/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NHibernate.HierarchyId")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MS SQL HierarchyId methods support for NHibernate")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Artur Kraev")]
 [assembly: AssemblyProduct("NHibernate.HierarchyId")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
